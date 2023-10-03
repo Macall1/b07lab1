d97b71afac00a03775f6125c6d6a1f409974604f
Michael Kim <mkmichael.kim@mail.utoronto.ca>
2023-10-02 23:37:00 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,12 +1,17 @@
 public class Driver {
 	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
+		
+		double [] c1 = {6,-5,-4,5};
+		int [] c2 = {2,1,3,0};
+		double [] c3 = { 1, 2, 3};
+		int [] c4 = {3,4,1};
+		Polynomial p1 = new Polynomial(c1, c2);
+		Polynomial p2 = new Polynomial(c3,c4);
+		Polynomial s = p1.multiply(p2);
+		for(int i = 0; i < s.exp.length; i++){
+			System.out.println(s.exp[i]);
+			System.out.println(s.nonzero[i]);
+		}
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
--- Polynomial.java
+++ Polynomial.java
@@ -1,62 +1,147 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.lang.Math;
+
+
 public class Polynomial{
-	double[] coefficient;
+	double[] nonzero;
+	int[] exp;
 	
 	public Polynomial(){
-		coefficient = new double[1];
-		coefficient[0] = 0;
+		nonzero = null;
+		exp = null;
 	}
 	
-	public Polynomial(double[] arg){
-		coefficient = arg;
+	public Polynomial(double[] arg, int[] exp){
+		nonzero = arg;
+		this.exp = exp;
 	}
 	
 	public Polynomial add(Polynomial poly){
 		Polynomial tmp;
-		
-		if (coefficient.length >= poly.coefficient.length){
-			tmp = new Polynomial(coefficient);
-			for(int i = 0; i < poly.coefficient.length-1; i++){
-				tmp.coefficient[i] = tmp.coefficient[i] + poly.coefficient[i];
-			} 
-		}
-		else{
-			tmp = new Polynomial(poly.coefficient);
-			for(int i = 0; i < poly.coefficient.length-1; i++){
-				tmp.coefficient[i] = coefficient[i] + tmp.coefficient[i];
+		int []arr1;
+		int a = poly.exp.length;
+		int b = 0;
+		for(int i = 0; i < exp.length; i++){
+			for(int j = 0; j < poly.exp.length; j++){
+				if(exp[i] == poly.exp[j]){
+					b = 1;
+				}
 			}
-		}	
+			if(b == 0){
+				a = a+1;
 
+			}
+			b = 0;	
+		}
+		arr1 = new int[a];
+		for(int i = 0; i < exp.length; i++){
+			arr1[i] = exp[i];
+		}
+		a = exp.length;
+		b = 0;
+		for(int i = 0; i < poly.exp.length; i++){
+			for(int j = 0; j < exp.length; j++){
+				if(poly.exp[i]==exp[j]){
+					b = 1;
+				}
+			}
+			if(b== 0){
+				arr1[a] = poly.exp[i];
+				a = a+1;
+			}
+			b = 0;
+		}
+		double[]arr2 = new double[arr1.length];
+		b=0;
+		a = 0;
+		for(int i = 0; i < arr2.length; i++){
+			for(int j = 0; j < poly.exp.length; j++){
+				if(arr1[i]==poly.exp[j]){
+					arr2[i] = arr2[i] + poly.nonzero[j];
+				}
+			}
+			for(int j= 0; j < exp.length; j++){
+				if(arr1[i] == exp[j]){
+					arr2[i] = arr2[i] + nonzero[j];
+				}
+			}
+		}
+		tmp = new Polynomial(arr2, arr1);
 		return tmp;
 	}
 	public double evaluate(double val){
-		double tmp;
-		double tmp2;
-		tmp = coefficient[0];
-		tmp2 = val;
-		for(int i = 1; i < coefficient.length; i++){
-			for(int j = 1; j < i; j++){
-				tmp2 = tmp2*val;
-			}
-			tmp = tmp + tmp2*coefficient[i];
+		double tmp = 0;
+		double tmp2 = 0;
+		for(int i = 0; i < exp.length;i++){
+			tmp2 = Math.pow(val, exp[i]);
+			tmp = tmp + tmp2*nonzero[i];
 		}
 		return tmp;
 	}
 	public boolean hasRoot(double arg){
-		double tmp;
-		double tmp2;
-		tmp = coefficient[0];
-		tmp2 = arg;
-		for(int i = 1; i < coefficient.length; i++){
-			for(int j = 1; j < i; j++){
-				tmp2 = tmp2*arg;
-			}
-			tmp = tmp + tmp2*coefficient[i];
-		}
+		double tmp = 0;
+		double tmp2 = 0;
+		for(int i = 0; i < exp.length;i++){
+			tmp2 = Math.pow(arg, exp[i]);
+			tmp = tmp + tmp2*nonzero[i];
+		}		
+
 		if (tmp == 0)
 			return true;
 		else 
 			return false;
 
 	}
+	public int[] arrayadd(int[] arr1, int[] arr2, int ind){
+		int [] arr3 = new int[arr1.length];
+		for(int i = 0; i < arr1.length; i++){
+			arr3[i] = arr1[i] + arr2[ind];
+		}
+		return arr3;
+	}
+
+	public double[] arradd(double[] arr1, double[] arr2, int ind){
+		double[] arr3 = new double[arr1.length];
+		for(int i = 0; i < arr1.length; i++){
+			arr3[i] = arr1[i] + arr2[ind];
+		}
+		return arr3;
+	}
+	
+
+    
+	public Polynomial multiply(Polynomial arg){
+		
+		Polynomial py;
+		double[]coeff = new double[nonzero.length];
+		int[]pow = new int[exp.length];
+		for(int i = 0; i < nonzero.length; i++){
+			coeff[i] = nonzero[i] * arg.nonzero[0];
+			pow[i] = exp[i] + arg.exp[0];
+		}
+		py = new Polynomial(coeff, pow);
+		for(int j = 1; j < arg.nonzero.length; j++){
+			double[] coeff1 = new double[nonzero.length];
+			int[] pow1 = new int[exp.length];
+
+			for(int i = 0; i < nonzero.length; i++ )
+			{
+				coeff1[i] = nonzero[i] * arg.nonzero[j];
+				pow1[i] = exp[i] + arg.exp[j];
+			}
+			
+			Polynomial py2 = new Polynomial(coeff1, pow1);
+			py = py.add(py2);
+			
+		}
+		return py;
+	}
+
+
+
+
 	
 }
